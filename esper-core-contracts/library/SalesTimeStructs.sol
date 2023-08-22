099c3ed4766f5b288cc19b9a10400a4d92342b1b
rwin <erwinphanglius10@gmail.com>
2023-08-22 13:28:17 +0700

    init public

---
+++ esper-core-contracts/library/SalesTimeStructs.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.7.6;
+pragma abicoder v2;
+
+library SalesTimeStructs {
+  struct SalesTime {
+    uint256 startTime;
+    uint256 endTime;
+  }
+}
